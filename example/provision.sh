d69fce4eab1164e691fe94a11115d2f7c1063246
Tom George <tg82490@gmail.com>
2019-02-26 22:48:54 +0000

    separate into separate directories

--- example/provision.sh
+++ example/provision.sh
@@ -1,4 +1,3 @@
-kubectl apply -f snapshot-serviceaccount-clusterrole-rolebinding.yaml
-kubectl apply -f storageclass.yaml
-kubectl apply -f pv.yaml -f pvc.yaml
-kubectl apply -f hostpath-snapshotter-deployment.yaml
+oc apply -f snapshot-serviceaccount-clusterrole-rolebinding.yaml
+oc apply -f storageclass.yaml
+
